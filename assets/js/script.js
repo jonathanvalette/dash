e6fcb89c5ba8835258214f8c88d0e5cee30e7bf5
jonathanvalette <jonathanvalette1204@gmail.com>
2021-07-09 09:42:12 +0200

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,3 @@
-
   /* Function for opening navbar on mobile */
 function toggleNavbar(collapseID) {
     document.getElementById(collapseID).classList.toggle("hidden");
@@ -6,24 +5,3 @@ function toggleNavbar(collapseID) {
   }
 
 
-  var mySwiper = new Swiper ('.swiper-container', {
-    // Optional parameters
-    direction: 'horizontal',
-    loop: true,
-
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-    },
-
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-
-    // And if we need scrollbar
-    scrollbar: {
-      el: '.swiper-scrollbar',
-    },
-  })
